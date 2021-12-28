9654cf490c90ddcd074d4830a6d1365f3fa01f39
elvisjacob <elvisjacob@debian-BULLSEYE-live-builder-AMD64>
2021-12-29 01:10:02 +0530

    added apis path names

---
+++ apis.md
@@ -0,0 +1,3 @@
+http://athul.local:8000/api/method/get_bus_timings
+http://athul.local:8000/api/method/get_all_stops
+http://athul.local:8000/api/method/get_all_bus
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,4 @@
+1
 <template>
 <button @click="locate">locate</button>
   <div v-if="location">
@@ -5,13 +6,13 @@
 </div>
  
 <button @click="filter">Filter</button>
-
+<button @click="loadData">locate</button>
 </template>
 
 <script>
 
 import L from 'leaflet';
-
+import axios from 'axios';
 export default {
   name: "Home",
   components: {},
@@ -19,22 +20,26 @@ export default {
     return {
       location :null,
       distance : 0,
-      coo : [
-        {lat: '11111', long:'11111'},
-        {lat: '22222', long:'22222'},
-        {lat: '33333', long:'33333'},
-        {lat: '44444', long:'44444'}
+      corrs : [
+        {lat: '111111', lon: '11111'},
+        {lat: '222222', lon: '22222'},
+        {lat: '333333', lon: '33333'},
+        {lat: '444444', lon: '44444'},
       ]
     }
   },
 
   methods: {
+    async loadData(){
+      var data = await axios.get('http://athul.local:8000/api/method/get_bus_timings')
+      console.log(data)
+    },
       
     locate(){
        if(navigator.geolocation){
          navigator.geolocation.getCurrentPosition(position => {
-           console.log(position.latitude)
-           console.log(position.longitude)
+           console.log(position)
+          this.location = position.coords
           },
          err => {
            console.log(err);
@@ -43,18 +48,15 @@ export default {
        }else {
          console.log("Your brower doesnt npt support")
        }
-       this.calDist();
+       
     },
     calDist(lat1, lon1, lat2, lon2){
       let l1 = L.latLng(lat1, lon1);
       let l2 = L.latLng(lat2,lon2)
       console.log(l1.distanceTo(l2)/ 1000);
     },
-
     filter(){
-      this.coo.forEach(item => {
-        this.calDist(this.position.latitude, this.position.longitude, item.lat, item.long)
-      });
+
     }
   }
 };
